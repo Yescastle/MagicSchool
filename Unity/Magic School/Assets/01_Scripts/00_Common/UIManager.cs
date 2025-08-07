d083eafec7e95f79a81b5f3293896e4ddb38c218
Yescastle <yescastle10@naver.com>
2025-08-07 17:24:32 +0900

    유니티 구현 업데이트

--- Unity/Magic School/Assets/01_Scripts/00_Common/UIManager.cs
+++ Unity/Magic School/Assets/01_Scripts/00_Common/UIManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -15,28 +16,49 @@ public class UIManager : MonoBehaviour
             InstanceUI = this;
     }
 
+    // 시작/종료 관련 메서드
     public void ToStartMenu()
     {
+        // 시작 메뉴로 이동
         SceneManager.LoadScene("StartMenu");
     }
-
     public void ToSelectPlayer()
     {
+        // 플레이어 선택창으로 이동
         SceneManager.LoadScene("SelectCharacter");
     }
-
     public void ToGame()
+    {
+        // (변경 예정): 현재 본 메서드는 "World" 씬으로 연결되도록 설정.
+        SceneManager.LoadScene("World");
+    }
+    public void ExitGame()
+    {
+        // 빌드 상에서 게임 종료
+        Application.Quit();
+
+        // 유니티 에디터 상에서 플레이 모드 종료
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    // 맵 간 이동 메서드
+    public void GoToSchool()
+    {
+        SceneManager.LoadScene("School");
+    }
+    public void GoToMarketplace()
+    {
+        SceneManager.LoadScene("Marketplace");
+    }
+    public void GoToVillage()
+    {
+        SceneManager.LoadScene("Village");
+    }
+    public void GoToWorld()
     {
         SceneManager.LoadScene("World");
     }
 
-    public void Setting()
-    {
-
-    }
-
-    public void ExitGame()
-    {
-
-    }
 }
